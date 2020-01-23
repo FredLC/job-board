a1624b37d268371e1152cc234ea32de165c04b86
fredlc <fredlefevre@outlook.com>
2020-01-23 14:35:59 -0500

    Created simple filtering algorithm to retrieve junior jobs only

--- workers/tasks/fetchGithub.js
+++ workers/tasks/fetchGithub.js
@@ -5,6 +5,7 @@ const { promisify } = require('util');
 const setAsync = promisify(client.set).bind(client);
 
 async function fetchGithub() {
+  // get all jobs
   let onPage = 0;
   let resultCount = 1;
   const baseUrl = 'https://jobs.github.com/positions.json?&page=';
@@ -18,8 +19,28 @@ async function fetchGithub() {
     onPage++;
   }
   console.log(`${allJobs.length} jobs total`);
-  const success = await setAsync('github', JSON.stringify(allJobs));
+
+  // filtering algorithm
+  const juniorJobs = allJobs.filter(job => {
+    const jobTitle = job.title.toLowerCase();
+
+    if (
+      jobTitle.includes('senior') ||
+      jobTitle.includes('sr.') ||
+      jobTitle.includes('manager') ||
+      jobTitle.includes('architect')
+    ) {
+      return false;
+    }
+    return true;
+  });
+
+  // set redis
+  const success = await setAsync('github', JSON.stringify(juniorJobs));
   console.log(success);
+  console.log(
+    `filtered down from ${allJobs.length} jobs to ${juniorJobs.length} jobs`
+  );
 }
 
 fetchGithub();
